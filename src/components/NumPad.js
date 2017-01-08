8f90c29bd7ba6429016a272183750c9f7aa72270
kevin940726 <kevin830726@gmail.com>
2017-01-08 21:44:38 +0800

    version 1 (not good enough)

--- src/components/NumPad.js
+++ src/components/NumPad.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import onClickOutside from 'react-onclickoutside';
-import styles from './NumPad.css';
+import style from './NumPad.css';
 import { range } from 'lodash';
+import { Motion, spring } from 'react-motion';
+
+const angleToCoor = (num, r) => ([
+  `${Math.cos(Math.PI * 2 / 9 * (num - 1) - Math.PI / 2) * r}px`,
+  `${Math.sin(Math.PI * 2 / 9 * (num - 1) - Math.PI / 2) * r}px`,
+].join(','));
 
 const NumPad = ({ handleClickOutside, handleClick }) => (
-  <div>
-    <div className={styles.numPad}>
-      {range(0, 10).map(num => (
-        <span
-          className={styles.num}
-          key={num}
-          onClick={() => handleClick(num)}
-        >
-          {num}
-        </span>
+  <div className={style.container}>
+    <div className={style.numPad}>
+      <span
+        className={style.num}
+        key="0"
+        onClick={() => handleClick(0)}
+      >X</span>
+      {range(1, 10).map(num => (
+        <Motion key={num} defaultStyle={{ r: 0 }} style={{ r: spring(55, {stiffness: 200, damping: 15}) }}>
+          {interpolatingStyle => (
+            <span
+              className={style.num}
+              key={num}
+              onClick={() => handleClick(num)}
+              style={{ transform: `translate(${angleToCoor(num, interpolatingStyle.r)})` }}
+            >
+              {num}
+            </span>
+          )}
+        </Motion>
       ))}
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="0" width="0">
       <defs>
         <filter id="goo">
           <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
